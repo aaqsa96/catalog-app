c55ed37cc1649db39d70ddc74455c346755eac91
Aqsa <aaqsa967@gmail.com>
2021-05-22 14:03:30 +0500

    theme extracted done

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/pages/home.dart';
+import 'package:flutter_catalog_app/pages/login_page.dart';
 import 'package:flutter_catalog_app/utils/routes.dart';
-import 'pages/home.dart';
-import 'pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catalog_app/widgets/themes.dart';
 
-void main() {
-  runApp(MaterialApp(
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
     title: 'Catalog App',
     //home: HomePage(),
     themeMode: ThemeMode.light,
-    theme: ThemeData(
-      primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
-    ),
-    darkTheme: ThemeData(
-      brightness: Brightness.dark,
-    ),
-    initialRoute: '/',
+    theme: MyTheme.lightTheme(context),
+    darkTheme: MyTheme.darkTheme(context),
+    initialRoute: MyRoutes.homeRoute,
     routes: {
       '/': (context) => LoginPage(),
       MyRoutes.loginRoute: (context) => LoginPage(),
       MyRoutes.homeRoute: (context) => HomePage(),
     },
-  ));
+  )
+  }
 }
