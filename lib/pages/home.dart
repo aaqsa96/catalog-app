3ff108e3596902127cd5694b00929528ca947514
Aqsa <aaqsa967@gmail.com>
2021-05-24 02:09:08 +0500

    day5 completed with fetching local jason data in listview

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,49 +14,6 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadData();
-//   }
-
-//   loadData() async {
-//     await Future.delayed(Duration(seconds: 2));
-//     final catalogJson =
-//         await rootBundle.loadString("assets/files/catalog.json");
-//     final decodedData = jsonDecode(catalogJson);
-//     var productsData = decodedData["products"];
-//     CatalogModel.items = List.from(productsData)
-//         .map<Item>((item) => Item.fromMap(item))
-//         .toList();
-//     setState(() {});
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       drawer: MyDrawer(),
-//       appBar: AppBar(
-//         title: Text('Catalog App'),
-//         centerTitle: true,
-//       ),
-//        body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-//             ? ListView.builder(
-//                 itemCount: CatalogModel.items.length,
-//                 itemBuilder: (context, index) => ItemWidget(
-//                   item: CatalogModel.items[index],
-//                 ),
-//               )
-//             : Center(
-//                 child: CircularProgressIndicator(),
-//               ),
-//       ),
-//     );
-//   }
-// }
-
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
