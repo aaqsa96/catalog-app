0138b6600eb36585ad3aa65d7e7894082aa760f6
Aqsa <aaqsa967@gmail.com>
2021-05-24 01:59:03 +0500

    day4 completed with local json fetching

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,21 +1,103 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:flutter_catalog_app/widgets/drawer.dart';
+
+import '../models/catalog.dart';
+import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadData();
+//   }
+
+//   loadData() async {
+//     await Future.delayed(Duration(seconds: 2));
+//     final catalogJson =
+//         await rootBundle.loadString("assets/files/catalog.json");
+//     final decodedData = jsonDecode(catalogJson);
+//     var productsData = decodedData["products"];
+//     CatalogModel.items = List.from(productsData)
+//         .map<Item>((item) => Item.fromMap(item))
+//         .toList();
+//     setState(() {});
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: MyDrawer(),
+//       appBar: AppBar(
+//         title: Text('Catalog App'),
+//         centerTitle: true,
+//       ),
+//        body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+//             ? ListView.builder(
+//                 itemCount: CatalogModel.items.length,
+//                 itemBuilder: (context, index) => ItemWidget(
+//                   item: CatalogModel.items[index],
+//                 ),
+//               )
+//             : Center(
+//                 child: CircularProgressIndicator(),
+//               ),
+//       ),
+//     );
+//   }
+// }
+
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    /*List<Item> list*/ CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    print(productsData);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MyDrawer(),
       appBar: AppBar(
-        title: Text('Catalog App'),
-        centerTitle: true,
+        title: Text("Catalog App"),
       ),
-      body: Center(child: Text('home page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatalogModel.items[index],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+      drawer: MyDrawer(),
     );
   }
 }
